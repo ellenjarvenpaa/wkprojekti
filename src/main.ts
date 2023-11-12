9446eacb834117d94cbcbf3a2db843e166c2e275
liem nguyen <elminguyen@gmail.com>
2023-11-12 23:55:36 +0200

    add html css js for menu item, login dialog, not done yet

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,17 @@
 // select dialog element from DOM
-const menuItemDialog = document.querySelector('dialog') as HTMLDialogElement | null;
+const menuItemDialog = document.querySelector('#product-info-dialog') as HTMLDialogElement | null;
+const loginDialog = document.querySelector('#login-dialog') as HTMLDialogElement | null;
 
 // select menu item elements from DOM
-const menuItemEls = document.querySelector('.menu-item') as HTMLElement | null;
+const menuItemEls = document.querySelectorAll('.menu-item');
 
-menuItemEls?.addEventListener('click', () => {
-	menuItemDialog?.showModal();
+menuItemEls.forEach((item) => {
+	item.addEventListener('click', () => {
+		menuItemDialog?.showModal();
+	})
+});
+
+const profileIconE = document.querySelector('#profile-icon') as HTMLElement | null;
+profileIconE?.addEventListener('click', () => {
+	loginDialog?.showModal();
 });
