afcad90cb094b3450431b799c821239097b399f6
liem nguyen <elminguyen@gmail.com>
2023-11-17 00:37:29 +0200

    create login-admin page

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,18 @@
 // vite.config.js
+import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 
 export default defineConfig({
   base: './',
-
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        admin: resolve(__dirname, 'login-admin.html')
+      },
+    },
+  },
 
 })
+
